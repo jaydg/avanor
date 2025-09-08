jaydg/avanor

commit 7a9a6a01c5e1ffdbcfc2a4db434a0c316f4ab3ff

Use std.string in XClassInfo and XClassFactory

creature/xhero.cpp
@@ -2402,7 +2402,7 @@ void XHero::QuickPay()
     XCreature * shopkeeper = NULL;
 	XAnyPlace * pl = l->map->GetPlace(x, y);
 	//hack!!!
-	if (pl == NULL || strcmp(pl->GetClassName(), "XShop") != 0)
+	if (pl == NULL || (pl->GetClassName() == "XShop"))
 	{
 		msgwin.Add("You can pay only in shops.");
 		return;

engine/xobject.cpp
@@ -52,7 +52,7 @@ XGUID guid = 1;
 XClassInfo * XClassFactory::first_class = NULL;
 int XClassFactory::counter = 0;
 
-XClassFactory::XClassFactory(char * name, CLASS_CREATOR pClassCreator, CLASS_CREATOR pClassNew)
+XClassFactory::XClassFactory(std::string name, CLASS_CREATOR pClassCreator, CLASS_CREATOR pClassNew)
 {
 	if (!first_class)
 		XClassFactory::first_class = new XClassInfo(name, pClassCreator, pClassNew);
@@ -80,24 +80,24 @@ XClassFactory::~XClassFactory()
 	}
 }
 
-XObject * XClassFactory::Create(char * name)
+XObject * XClassFactory::Create(std::string name)
 {
 	XClassInfo * tmp = XClassFactory::first_class;
 	while(tmp)
 	{
-		if (strcmp(tmp->name, name)==0) return tmp->pClassCreator();
+		if (tmp->name == name) return tmp->pClassCreator();
 		tmp = tmp->next;
 	}
 	assert(0);
 	return NULL;
 }
 
-XObject * XClassFactory::CreateNew(char * name)
+XObject * XClassFactory::CreateNew(std::string name)
 {
 	XClassInfo * tmp = XClassFactory::first_class;
 	while(tmp)
 	{
-		if (strcmp(tmp->name, name)==0) return tmp->pClassNew();
+		if (tmp->name == name) return tmp->pClassNew();
 		tmp = tmp->next;
 	}
 	return NULL;
@@ -144,9 +144,9 @@ void XObject::StoreAllObjects(XFile * f)
 	FILE * tmp = fopen("dmp.txt", "wt");
 	for(i = 0; i < count; i++) 
 	{
-		unsigned char name_size = strlen(table[i]->GetClassName());
+		unsigned char name_size = table[i]->GetClassName().size();
 		f->Write(&name_size, sizeof(name_size));
-		f->Write(table[i]->GetClassName(), sizeof(char), name_size);
+		f->Write(table[i]->GetClassName().c_str(), sizeof(char), name_size);
 		table[i]->bAlreadyStored = false;
 		fprintf(tmp, "[%d] %s\n", i, table[i]->GetClassName());
 	}

engine/xobject.h
@@ -27,6 +27,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include <assert.h>
 #include "defs.h"
 #include "xfile.h"
+#include <string>
 
 
 enum ITEM_MASK {
@@ -91,9 +92,9 @@ struct DUMMY_STRUCT
 #define DECLARE_CREATOR(__xClass, __xBaseClass) \
 	__xClass(DUMMY_STRUCT * ds) : __xBaseClass(ds) {} \
 	static __xClass * Creator() {DUMMY_STRUCT ds; return new __xClass(&ds);} \
-	static __xClass * MakeNew() { return new __xClass(); }	\
-	virtual const char * GetClassName() {return #__xClass;} 
-	
+	static __xClass * MakeNew() { return new __xClass(); } \
+	virtual const std::string GetClassName() {return #__xClass;}
+
 //	void InvalidateLeave() {__xBaseClass::Invalidate();}
 
 //#define INVALIDATE_ENTER() static int inside = 0; assert(!inside); if (!isValid()) return; inside = 1;
@@ -104,8 +105,8 @@ struct DUMMY_STRUCT
 class XClassInfo
 {
 public:
-	XClassInfo(char * _name, CLASS_CREATOR p, CLASS_CREATOR n) {name = _name; pClassCreator = p; pClassNew = n; next = NULL;}
-	char * name;
+	XClassInfo(const std::string _name, CLASS_CREATOR p, CLASS_CREATOR n) {name = _name; pClassCreator = p; pClassNew = n; next = NULL;}
+	std::string name;
 	CLASS_CREATOR pClassCreator;
 	CLASS_CREATOR pClassNew;
 	XClassInfo * next;
@@ -116,10 +117,10 @@ class XClassFactory
 public:
 	static XClassInfo * first_class;
 	static int counter;
-	XClassFactory(char * name, CLASS_CREATOR pClass, CLASS_CREATOR pClassNew);
+	XClassFactory(const std::string name, CLASS_CREATOR pClass, CLASS_CREATOR pClassNew);
 	~XClassFactory();
-	static XObject * Create(char * name);
-	static XObject * CreateNew(char * name);
+	static XObject * Create(const std::string name);
+	static XObject * CreateNew(const std::string name);
 };
 
 #define DYNCREATE(x) XClassFactory::Create(x)
@@ -244,7 +245,7 @@ public:
 		o->Invalidate();
 	}
 	
-   virtual const char * GetClassName() { return "XObject"; }
+   virtual const std::string GetClassName() { return "XObject"; }
    XObject * MakeNew() { assert(0); return NULL; }
    
    virtual void Dump(XFile * f);

commit d9dee1b829d0d2f2d746043ce7195fe2c08970ef

Fix paying in shops
Regression introduced in 7a9a6a0.

creature/xhero.cpp
@@ -2426,7 +2426,7 @@ void XHero::QuickPay()
     XAnyPlace * pl = l->map->GetPlace(x, y);
 
     //hack!!!
-    if (pl == NULL || (pl->GetClassName() == "XShop")) {
+    if (pl == NULL || (pl->GetClassName() != "XShop")) {
         msgwin.Add("You can pay only in shops.");
         return;
     }