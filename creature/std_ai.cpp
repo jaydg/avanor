jaydg/avanor

commit 69faf6f9434cdecb58b2cc365c22d667565ef3f2

Port XItemList to std::set

creature/std_ai.cpp
@@ -535,11 +535,8 @@ void XStandardAI::SetEnemyClass(CREATURE_CLASS cr_class)
 
 int XStandardAI::Wear()
 {
-    it_iterator it;
 
-    for (it = ai_owner->contain.begin(); it != ai_owner->contain.end(); it++) {
-        XItem * item = static_cast<XItem*>(static_cast<XObject*>(it));
-
+    for (auto item: ai_owner->contain) {
         XBodyPart * xbp = ai_owner->GetBodyPart(item->bp);
 
         if (!xbp) {
@@ -572,11 +569,11 @@ int XStandardAI::Wear()
         }
 
         if (old_item) {
-            ai_owner->contain.Add(xbp->UnWear());
+            ai_owner->contain.insert(xbp->UnWear());
         }
 
         xbp->Wear(item);
-        ai_owner->contain.Remove(it);
+        ai_owner->contain.erase(item);
 
         if (ai_owner->isVisible()) {
             char xbuf[256];
@@ -607,11 +604,7 @@ int XStandardAI::Wear()
     // Sacrifice useless items
     int s_flag = 0;
 
-    it = ai_owner->contain.begin();
-
-    while (it != ai_owner->contain.end()) {
-        XItem * item = it;
-        it++;
+    for (const auto item: ai_owner->contain) {
         assert(item->isValid());
 
         if (item->GetValue() > 800) {
@@ -626,8 +619,7 @@ int XStandardAI::Wear()
             continue;
         }
 
-        it--;
-        it = ai_owner->contain.erase(it);
+        ai_owner->contain.erase(item);
         ai_owner->Sacrifice(item);
         s_flag = 1;
         break;
@@ -831,12 +823,12 @@ int XStandardAI::CastSpell() const {
 
 int XStandardAI::ReadScroll()
 {
-    for (it_iterator it = ai_owner->contain.begin(); it != ai_owner->contain.end(); it++) {
-        if (!(it->im & IM_SCROLL)) {
+    for (auto item: ai_owner->contain) {
+        if (!(item->im & IM_SCROLL)) {
             continue;
         }
 
-        XScroll * scroll = static_cast<XScroll*>(static_cast<XObject*>(it));
+        XScroll* scroll = static_cast<XScroll*>(item);
 
         if (scroll->sc_name == SCROLL_MAGIC_ARROW ||
             scroll->sc_name == SCROLL_FIRE_BOLT ||
@@ -859,25 +851,23 @@ int XStandardAI::ReadScroll()
 int XStandardAI::DrinkPotion()
 {
     if (ai_owner->_HP < ai_owner->GetMaxHP() / 3) {
-        for (it_iterator i = ai_owner->contain.begin(); i != ai_owner->contain.end(); i++) {
-            XItem * it = i;
-
+        for (const auto it: ai_owner->contain) {
             if (it->im & IM_POTION) {
-                XPotion * pot = (XPotion*)it;
+                auto pot = static_cast<XPotion *>(it);
 
                 if (pot->pn == PN_HEALING ||
                     pot->pn == PN_CURE_LIGHT_WOUNDS ||
                     pot->pn == PN_CURE_SERIOUS_WOUNDS ||
                     pot->pn == PN_CURE_CRITICAL_WOUNDS ||
                     pot->pn == PN_CURE_MORTAL_WOUNDS) {
-                    XPotion * np = (XPotion*)pot->MakeCopy();
+                    // FIXME: why the copy?
+                    XPotion * np = static_cast<XPotion *>(pot->MakeCopy());
                     np->onDrink(ai_owner);
 
                     if (pot->quantity > 1) {
                         pot->quantity--;
                     } else {
-                        XObject * obj = ai_owner->contain.Remove(i);
-                        assert(obj->guid() == pot->guid());
+                        ai_owner->contain.erase(it);
                         pot->Invalidate();
                     }
 
@@ -916,21 +906,17 @@ int XStandardAI::PickUpItems()
     XItemList * item_list = ai_owner->l->map->GetItemList(ai_owner->x, ai_owner->y);
     bool item_picked = false;
 
-    it_iterator it = item_list->begin();
-
-    while (it != item_list->end()) {
+    for (auto it: *item_list) {
         if (it->im & IM_CHEST) {
             break;
         }
 
-        XItem * tit = it;
-        it = item_list->erase(it);
+        item_list->erase(it);
 
-        if (ai_owner->PickUpItem(tit)) {
+        if (ai_owner->PickUpItem(it)) {
             item_picked = true;
-            continue;
         } else {
-            item_list->Add(tit);
+            item_list->insert(it);
             break;
         }
     }
@@ -1187,15 +1173,10 @@ void XStandardAI::RunScript()
         break;
 
         case SCC_DROP_ITEM: {
-            XItemList::iterator it = ai_owner->contain.begin();
-
-            while (it != ai_owner->contain.end()) {
-                if (it->im & cmd.im) {
-                    XItem * item = it;
-                    it = ai_owner->contain.erase(it);
+            for (auto item: ai_owner->contain) {
+                if (item->im & cmd.im) {
+                    ai_owner->contain.erase(item);
                     ai_owner->DropItem(item);
-                } else {
-                    it++;
                 }
             }
 

creature/creature.cpp
@@ -91,7 +91,9 @@ XCreature::XCreature()
 
 void XCreature::Invalidate()
 {
-    contain.KillAll();
+    for (auto item: contain) {
+        item->Invalidate();
+    }
 
     for (auto component: components)
         component->Invalidate();
@@ -194,7 +196,7 @@ int XCreature::stopAction()
         action_data.item->onUse(UIS_STOP, this);
     } else {
         if (action_data.item) {
-            contain.Add(action_data.item.get());
+            contain.insert(action_data.item.get());
         }
     }
 
@@ -896,9 +898,7 @@ void XCreature::Die(XCreature * killer)
         }
     }
 
-    XItem * item;
-
-    while ((item = contain.RemoveFirst()) != NULL) {
+    for (auto item: contain) {
         item->Drop(l, x, y);
     }
 
@@ -952,7 +952,7 @@ int XCreature::PickUpItem(XItem * i)
             if (xbp && xbp->Item() && xbp->Item()->Compare(i) == 0 && i->GetRef() == 0) {
                 xbp->Item()->Concat(i);
             } else {
-                contain.Add(i);
+                contain.insert(i);
             }
 
             return 1;
@@ -1522,7 +1522,9 @@ void XCreature::Store(XFile * f)
 
     action_data.Store(f);
 
-    contain.StoreList(f);
+    // FIXME: Implement when porting saving/restoring to Cereal
+    // contain.StoreList(f);
+
     religion.Store(f);
     max_stats.Store(f);
     f->Write(&creature_person_type, sizeof(creature_person_type));
@@ -1604,7 +1606,9 @@ void XCreature::Restore(XFile * f)
     action_data.Restore(f);
 
     assert(contain.empty());
-    contain.RestoreList(f);
+    // FIXME: Implement when porting saving/restoring to Cereal
+    // contain.RestoreList(f);
+
     religion.Restore(f);
     max_stats.Restore(f);
     f->Read(&creature_person_type, sizeof(creature_person_type));
@@ -1702,7 +1706,7 @@ int XCreature::Chat(XCreature * chatter, const char* msg)
 bool XCreature::ContainItem(XItem * item)
 {
     if (CarryItem(item)) {
-        contain.Add(item);
+        contain.insert(item);
         return true;
     } else {
         return false;
@@ -1823,49 +1827,51 @@ int XCreature::onGiveItem(XCreature * giver, XItem * item)
 
 int XCreature::MoneyOp(int money_count)
 {
-    it_iterator it = contain.begin();
+    XItem* money = nullptr;
 
-    while (it != contain.end()) {
+    for (auto it: contain) {
         if (it->im & IM_MONEY) {
+            money = it;
             break;
         }
-
-        it++;
     }
 
-    if (it != contain.end()) {
-        XItem * money = it;
-
+    if (money) {
         if (money_count >= 0) {
             carried_weight -= money->quantity / 10;
             money->quantity += money_count;
             carried_weight += money->quantity / 10;
+
             return money->quantity;
-        } else {
-            if (money->quantity + money_count > 0) {
-                carried_weight -= money->quantity / 10;
-                money->quantity += money_count;
-                carried_weight += money->quantity / 10;
-                return money->quantity;
-            } else if (money->quantity + money_count == 0) {
-                carried_weight -= money->quantity / 10;
-                contain.Remove(it)->Invalidate();
-                return 0;
-            } else {
-                return money->quantity + money_count;
-            }
         }
-    } else {
-        if (money_count > 0) {
-            carried_weight += money_count / 10;
-            contain.Add(new XMoney(money_count));
-            return money_count;
-        } else {
-            return money_count;
+
+        if (money->quantity + money_count > 0) {
+            carried_weight -= money->quantity / 10;
+            money->quantity += money_count;
+            carried_weight += money->quantity / 10;
+
+            return money->quantity;
         }
+
+        if (money->quantity + money_count == 0) {
+            carried_weight -= money->quantity / 10;
+            money->Invalidate();
+            contain.erase(money);
+
+            return 0;
+        }
+
+        return money->quantity + money_count;
     }
 
-    return 0;
+    if (money_count > 0) {
+        carried_weight += money_count / 10;
+        contain.insert(new XMoney(money_count));
+
+        return money_count;
+    }
+
+    return money_count;
 }
 
 const char* XCreature::GetGenderStr()

creature/xhero.cpp
@@ -604,15 +604,15 @@ void XHero::Move()
             msgwin.Add("There is a heap of items here.");
         } else if (l->map->GetItemCount(nx, ny) == 1) {
             XItemList* ilist = (l->map->GetItemList(nx, ny));
-            XItem * ite = ilist->begin();
+            XItem* item = *(ilist->begin());
             char buf[256];
             msgwin.Add("There is a");
             XAnyPlace* place = l->map->GetPlace(nx, ny);
 
             if (place) {
-                place->onShowItem(ite, buf);
+                place->onShowItem(item, buf);
             } else {
-                ite->toString(buf);
+                item->toString(buf);
             }
 
             msgwin.Add(buf);
@@ -894,13 +894,13 @@ XItem* XHero::Inventory(XItemList* item_list, ITEM_MASK mask, const INVENTORY_FL
         list.SetFooter(MSG_LIGHTGRAY "filtr: " MSG_BROWN "[" MSG_YELLOW "[|{}'=!?\"\\%]$X" MSG_BROWN "]");
 
         //count items for show
-        it_iterator it;
         int all_item_count = 0;
 
-        for (it = item_list->begin(); it != item_list->end(); ++it)
-            if ((ifiltr && ifiltr(it)) || (it->im & mask)) {
+        for (const auto it : *item_list) {
+            if ((ifiltr && ifiltr(it)) || it->im & mask) {
                 all_item_count++;
             }
+        }
 
         char buf[256];
 
@@ -926,16 +926,16 @@ XItem* XHero::Inventory(XItemList* item_list, ITEM_MASK mask, const INVENTORY_FL
         } else {
             ITEM_MASK last_mask = IM_UNKNOWN;
 
-            for (it = item_list->begin(); it != item_list->end(); ++it) {
-                if ((ifiltr && ifiltr(it)) || (it->im & mask)) {
+            for (const auto item: *item_list) {
+                if ((ifiltr && ifiltr(item)) || (item->im & mask)) {
                     // we need to show item group name (e.g. boots, weapons etc.)
-                    if (it->im != last_mask) {
+                    if (item->im != last_mask) {
                         // skip output empty string for first item in inventory
                         if (last_mask != IM_UNKNOWN) {
                             list.AddItem(new XGuiItem_Text(""), 0);
                         }
 
-                        last_mask = it->im;
+                        last_mask = item->im;
 
                         for (int oi = 0; oi < std::size(output_items_name); oi++) {
                             if (output_items_mask[oi] & last_mask) {
@@ -946,7 +946,7 @@ XItem* XHero::Inventory(XItemList* item_list, ITEM_MASK mask, const INVENTORY_FL
                     }
 
                     //output item
-                    list.AddItem(new XGuiItem_Inventory(it), 0);
+                    list.AddItem(new XGuiItem_Inventory(item), 0);
                 }
             }
         }
@@ -979,11 +979,11 @@ XItem* XHero::Inventory(XItemList* item_list, ITEM_MASK mask, const INVENTORY_FL
                 break;
             }
         } else {
-            it_iterator selected_it = item_list->begin();
+            auto selected_it = item_list->begin();
             int stop_flag = -1;
 
             while (true) {
-                if ((ifiltr && ifiltr(selected_it)) || (selected_it->im & mask)) {
+                if ((ifiltr && ifiltr(*selected_it)) || ((*selected_it)->im & mask)) {
                     stop_flag++;
                 }
 
@@ -995,7 +995,9 @@ XItem* XHero::Inventory(XItemList* item_list, ITEM_MASK mask, const INVENTORY_FL
             }
 
             assert(stop_flag == item_number);
-            XItem* ritem = item_list->Remove(selected_it);
+
+            XItem* ritem = *selected_it;
+            item_list->erase(selected_it);
 
             if (ret_item_count <= 0) {
                 return ritem;
@@ -1003,13 +1005,15 @@ XItem* XHero::Inventory(XItemList* item_list, ITEM_MASK mask, const INVENTORY_FL
 
             if (ritem->quantity <= ret_item_count) {
                 return ritem;
-            } else {
-                auto sitem = dynamic_cast<XItem *>(ritem->MakeCopy());
-                sitem->quantity = ret_item_count;
-                ritem->quantity -= ret_item_count;
-                item_list->Add(ritem);
-                return sitem;
             }
+
+            auto sitem = dynamic_cast<XItem *>(ritem->MakeCopy());
+            sitem->quantity = ret_item_count;
+            ritem->quantity -= ret_item_count;
+            item_list->insert(ritem);
+
+            return sitem;
+
         }
     }
 
@@ -1106,7 +1110,7 @@ void XHero::Equipment(FILE * f)
 
             if (witem != nullptr) {
                 xqsa[n]->UnWear();
-                contain.Add(witem);
+                contain.insert(witem);
             } else {
                 if (xqsa[n]->GetProperIM() == IM_MISSILE) {
                     witem = Inventory(&contain, xqsa[n]->GetProperIM(), IF_FIXED_MASK);
@@ -1121,7 +1125,7 @@ void XHero::Equipment(FILE * f)
                         if (xqsa[n]->bp_uin == witem->bp) {
                             xqsa[n]->Wear(witem);
                         } else {
-                            contain.Add(witem);
+                            contain.insert(witem);
                         }
                     }
                 }
@@ -1177,11 +1181,11 @@ void XHero::ReadAll()
     if (item) {
         if (item->im & IM_SCROLL) {
             if (!XCreature::Read(item)) {
-                contain.Add(item);
+                contain.insert(item);
             }
         } else if (item->im & IM_BOOK) {
             if (!XCreature::Read(item)) {
-                contain.Add(item);
+                contain.insert(item);
             }
         }
     }
@@ -1219,7 +1223,7 @@ void XHero::DropItem()
             const int res = GetTarget(TR_HOW_MUCH, &pt, item->quantity);
 
             if (res == 0) {
-                contain.Add(item);
+                contain.insert(item);
                 break;
             }
 
@@ -1227,7 +1231,7 @@ void XHero::DropItem()
                 drop_item = dynamic_cast<XItem *>(item->MakeCopy());
                 drop_item->quantity = res;
                 item->quantity -= res;
-                contain.Add(item);
+                contain.insert(item);
             }
         }
 
@@ -1239,7 +1243,7 @@ void XHero::DropItem()
         }
 
         if (!XCreature::DropItem(drop_item)) {
-            contain.Add(drop_item);
+            contain.insert(drop_item);
             return;
         }
 
@@ -1274,7 +1278,9 @@ void XHero::PickItem()
             }
         }
     } else if (tmpquae->size() == 1) {
-        XItem * tit = tmpquae->RemoveFirst();
+        XItem* tit = *(tmpquae->begin());
+        tmpquae->erase(tit);
+
         char buf[256];
         tit->toString(buf);
 
@@ -1282,7 +1288,7 @@ void XHero::PickItem()
             sprintf(bufx, "You pick up a %s.", buf);
             msgwin.Add(bufx);
         } else {
-            tmpquae->Add(tit);
+            tmpquae->insert(tit);
         }
     } else {
         XItem * tit;
@@ -1296,7 +1302,7 @@ void XHero::PickItem()
             if (PickUpItem(tit)) {
                 nitem++;
             } else { //we can't pick item, so return it back
-                tmpquae->Add(tit);
+                tmpquae->insert(tit);
                 vRefresh();
                 vGetch();
             }
@@ -1346,7 +1352,7 @@ void XHero::OpenChest()
             }
         } while (it);
 
-        tq->Add(last_chest);
+        tq->insert(last_chest);
     } else if (chest_count == 1) {
         msgwin.Add("Do you wish to open the chest?");
 
@@ -1540,8 +1546,9 @@ int XHero::XShoot()
     XItem* missile = GetItem(BP_MISSILE);
     const auto missile_w = dynamic_cast<XMissileWeapon *>(GetItem(BP_MISSILE_WEAPON));
 
-    if (!missile) { //if no missile, try to load them
-        for (XItemList::iterator it = contain.begin(); it != contain.end(); ++it) {
+    // if no missile, try to load them
+    if (!missile) {
+        for (auto it: contain) {
             if (it->im & IM_MISSILE && XMissile::isProperWeapon(it, missile_w)) {
                 msgwin.ClrMsg();
                 msgwin.Add("Load");
@@ -2341,7 +2348,7 @@ XItem* XHero::onIdentifyItem()
     XItem * it = Inventory(&contain);
 
     if (it) {
-        contain.Add(it);
+        contain.insert(it);
     }
 
     return it;
@@ -2450,7 +2457,7 @@ void XHero::GiveItem()
 
     if (XItem* item = Inventory(&contain)) {
         if (item->im & IM_MONEY) {
-            contain.Add(item);
+            contain.insert(item);
             last_creature->xai->onGiveItem(this, item);
         } else {
             int res = item->quantity;
@@ -2467,7 +2474,7 @@ void XHero::GiveItem()
                 gitem = dynamic_cast<XItem *>(item->MakeCopy());
                 gitem->quantity = res;
                 item->quantity -= res;
-                contain.Add(item);
+                contain.insert(item);
             } else {
                 gitem = item;
             }
@@ -2479,7 +2486,7 @@ void XHero::GiveItem()
 
             if (!flag || res == 0) {
                 carried_weight += gitem->weight;
-                contain.Add(gitem);
+                contain.insert(gitem);
             }
         }
     }

creature/xhero3.cpp
@@ -67,7 +67,7 @@ void XHero::doSacrifice()
                 const int res = GetTarget(TR_HOW_MUCH, &pt, item->quantity);
 
                 if (res == 0) {
-                    contain.Add(item);
+                    contain.insert(item);
                     break;
                 }
 
@@ -75,7 +75,7 @@ void XHero::doSacrifice()
                     drop_item = dynamic_cast<XItem *>(item->MakeCopy());
                     drop_item->quantity = res;
                     item->quantity -= res;
-                    contain.Add(item);
+                    contain.insert(item);
                 }
             }
 
@@ -425,7 +425,7 @@ void XHero::MixPotions()
                 sprintf(buf1, "You have mixed %s.", buf);
                 msgwin.Add(buf1);
                 CarryItem(pot);
-                contain.Add(pot);
+                contain.insert(pot);
             } else {
                 msgwin.Add("You failed to mix a new potion.");
             }

engine/xobject.h
@@ -182,6 +182,13 @@ class XObject
         static void StoreAllObjects(XFile * f);
         static void RestoreAllObjects(XFile * f);
         static void InvalidateAllObjects();
+        static XObject* GetObject(XGUID guid) {
+            if (const auto it = objects.find(guid); it != objects.end()) {
+                return objects[guid];
+            }
+
+            return nullptr;
+        }
 
         XObject(DUMMY_STRUCT * ds) : is_valid(1)
         {

game/location.cpp
@@ -1225,30 +1225,30 @@ int XLocation::GiveObjectToCreature(lua_State * L)
 
 int XLocation::GiveAward(lua_State * L)
 {
-    XCreature * owner = (XCreature*)lua_topointer(L, 1);
-    XGUID object = lua_tonumber(L, 2);
-    XCreature * target = (XCreature*)lua_topointer(L, 3);
+    const auto owner = (XCreature*)lua_topointer(L, 1);
+    const XGUID aguid = lua_tonumber(L, 2);
+    const auto target = (XCreature*)lua_topointer(L, 3);
+    auto item = dynamic_cast<XItem *>(GetObject(aguid));
 
-    XItem * it = owner->contain.Find(object);
-
-    if (it) {
-        owner->contain.Remove(it->guid());
+    const auto it = owner->contain.find(item);
+    if (it != owner->contain.end()) {
+        owner->contain.erase(it);
     } else {
         for (const auto bp: owner->components) {
-            if (bp->Item() && bp->Item()->guid() == object) {
-                it = bp->UnWear();
+            if (bp->Item() && bp->Item() == item) {
+                item = bp->UnWear();
                 break;
             }
         }
     }
 
-    if (it) {
-        owner->UnCarryItem(it);
+    if (item) {
+        owner->UnCarryItem(item);
 
-        if (target->CarryItem(it)) {
-            target->contain.Add(it);
+        if (target->CarryItem(item)) {
+            target->contain.insert(item);
         } else {
-            owner->DropItem(it);
+            owner->DropItem(item);
         }
 
         lua_pushboolean(L, true);

game/shop.cpp
@@ -93,12 +93,10 @@ int XShop::onCreatureEnter(XCreature * cr)
     if (cr->isHero()) {
         for (int i = area.left + 1; i < area.right - 1; i++)
             for (int j = area.top + 1; j < area.bottom - 1; j++) {
-                XItemList * ilist = location->map->GetItemList(i, j);
-                it_iterator it;
+                XItemList* ilist = location->map->GetItemList(i, j);
 
-                for (it = ilist->begin(); it != ilist->end(); it++) {
-                    XItem * tit = static_cast<XItem*>(static_cast<XObject*>(it));
-                    tit->Identify(1);
+                for (auto it: *ilist) {
+                    it->Identify(1);
                 }
             }
     }

item/item.h
@@ -21,9 +21,10 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <set>
+
 #include "creature/bodypart.h"
 #include "engine/xbaseobj.h"
-#include "engine/xlist.h"
 #include "item/itemdb.h"
 #include "item/itemdef.h"
 
@@ -126,7 +127,10 @@ class XItem : public XBaseObject
         void GetArtifactName(char* buf, const char* real_name);
 };
 
-typedef XSortedList<XItem*>::iterator it_iterator;
-typedef XSortedList<XItem*> XItemList;
+inline bool compare::operator()(const XItem* lhs, const XItem* rhs) const {
+    return lhs->im < rhs->im;
+}
+
+typedef std::set<XItem*, compare> XItemList;
 
 #endif

commit 9a6d920d5ffa4fe9ab5384a1a4da754bb3b3bc8f

Fix XStandardAI::PickUpItems
Broken with 69faf6f9.

creature/std_ai.cpp
@@ -896,20 +896,21 @@ int XStandardAI::Shoot() const
 
 int XStandardAI::PickUpItems() const
 {
-    XItemList * item_list = ai_owner->l->map->GetItemList(ai_owner->x, ai_owner->y);
+    XItemList *item_list = ai_owner->l->map->GetItemList(ai_owner->x, ai_owner->y);
     bool item_picked = false;
 
-    for (auto it: *item_list) {
-        if (it->im & IM_CHEST) {
+    for (auto it = item_list->begin(); it != item_list->end(); ) {
+        if ((*it)->im & IM_CHEST) {
             break;
         }
 
-        item_list->erase(it);
+        XItem* current_item = *it;
+        it = item_list->erase(it);
 
-        if (ai_owner->PickUpItem(it)) {
+        if (ai_owner->PickUpItem(current_item)) {
             item_picked = true;
         } else {
-            item_list->insert(it);
+            item_list->insert(it, current_item);
             break;
         }
     }