jaydg/avanor

commit 61e256ef62459fc95dda2d3f45355e496d19e656

Use fmt to format highscrore items

helpers/hiscore.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #include <algorithm>
 #include <ctime>
+#include <fmt/format.h>
 #include <fstream>
 #include <cereal/archives/json.hpp>
 #include <cereal/types/memory.hpp>
@@ -30,26 +31,22 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 XGuiItem_Text* XHiScoreItem::toGuiItem()
 {
-    char buf[256];
-    char dbuf[256];
+    auto color = isLastRecord ? MSG_WHITE : MSG_CYAN;
 
-    if (flag == 0) {
-        sprintf(dbuf, "Died");
-    } else {
-        sprintf(dbuf, "Won");
-    }
+    auto text = fmt::format(
+        "{}{:3d} {} {:7d}  {} {} {} on {}/{}/{}.\n               {}\n\n",
+        color, place,
+        color, score,
+        color, name,
+        flag == 0 ? "Died" : "Won",
+        day, month, year,
+        msg
+    );
 
-    if (isLastRecord)
-        sprintf(buf, "%s%3d %s %7d  %s %s %s on %d/%d/%d.\n               %s\n\n",
-            MSG_WHITE, place, MSG_WHITE, score, MSG_WHITE, name, dbuf, day, month, year, msg);
-    else
-        sprintf(buf, "%s%3d %s %7d  %s %s %s on %d/%d/%d.\n               %s\n\n",
-            MSG_CYAN, place, MSG_CYAN, score, MSG_CYAN, name, dbuf, day, month, year, msg);
-
-    return new XGuiItem_Text(buf);
+    return new XGuiItem_Text(text.c_str());
 }
 
-XHiScoreItem::XHiScoreItem(const int _place, const unsigned int _score, const char* _name, const char* _msg, const int flg, const int last_record)
+XHiScoreItem::XHiScoreItem(const int _place, const unsigned int _score, std::string _name, std::string _msg, const int flg, const int last_record)
 {
     const time_t t = time(nullptr);
     const tm* _tm = gmtime(&t);
@@ -59,8 +56,8 @@ XHiScoreItem::XHiScoreItem(const int _place, const unsigned int _score, const ch
     day = _tm->tm_mday;
     score = _score;
     place = _place;
-    strcpy(name, _name);
-    strcpy(msg, _msg);
+    name = _name;
+    msg = _msg;
     isLastRecord = last_record;
     flag = flg;
 }

Makefile
@@ -48,7 +48,7 @@ OBJDIR = obj
 NAME = avanor
 
 CFLAGS += -I.
-CFLAGS += $(shell pkg-config --cflags luajit)
+CFLAGS += $(shell pkg-config --cflags fmt luajit)
 
 VPATH = creature engine game global helpers item magic map other lua
 
@@ -71,7 +71,7 @@ SRCS = xweapon.cpp xtool.cpp xtime.cpp xstring.cpp xshield.cpp                \
        bodypart.cpp anycr.cpp ai_view.cpp
 
 LIBS = -lncurses
-LIBS += $(shell pkg-config --libs luajit)
+LIBS += $(shell pkg-config --libs fmt luajit)
 
 ifdef debug
 	CFLAGS += -g

helpers/hiscore.h
@@ -33,7 +33,7 @@ class XFile;
 class XHiScoreItem
 {
     public:
-        XHiScoreItem(int place, unsigned int score, const char* _name, const char* _msg, int flg, int last_record = 0);
+        XHiScoreItem(int place, unsigned int score, std::string _name, std::string _msg, int flg, int last_record = 0);
         XHiScoreItem() : isLastRecord(0) {}
 
         XGuiItem_Text* toGuiItem();
@@ -60,8 +60,8 @@ class XHiScoreItem
         int year{};
         int day{};
         int month{};
-        char name[80]{};
-        char msg[160]{};
+        std::string name;
+        std::string msg;
         int isLastRecord;
         int flag{}; // win or death
 };

commit 23f27f057decd068bc0162b22606a24d94906dfc

Fix reading highscores
61e256ef broke the highscore file format: name/msg switched from fixed-size
char[] to std::string without a version bump, so old files with the char[]
layout were silently misread as the new std::string layout and crashed.

helpers/hiscore.cpp
@@ -62,7 +62,7 @@ XHiScoreItem::XHiScoreItem(const int _place, const unsigned int _score, std::str
     flag = flg;
 }
 
-#define HISCORE_VERSION 0xFFEEEE0C
+#define HISCORE_VERSION 0xFFEEEE0D
 
 XHiScore::XHiScore()
 {
@@ -74,14 +74,24 @@ XHiScore::XHiScore()
     }
 
     // file exists, we can read it
-    cereal::JSONInputArchive archive(file);
+    try {
+        cereal::JSONInputArchive archive(file);
 
-    int version;
+        int version;
+        archive(version);
 
-    archive(
-        version,
-        items
-    );
+        // an older or otherwise incompatible on-disk format:
+        // discard rather than misinterpret its fields.
+        if (version != HISCORE_VERSION) {
+            return;
+        }
+
+        archive(items);
+    } catch (const cereal::Exception&) {
+        // corrupt or unreadable file - start with an empty score list
+        // instead of crashing.
+        items.clear();
+    }
 }
 
 XHiScore::~XHiScore()